fb1bb051e0a313183b5d104448c70a0ad4e3eb92
Muhammed Enes <75534004+enes395@users.noreply.github.com>
2022-10-21 14:03:15 +0300

    Add files via upload

---
+++ NewsAppMVVM/Config/APIKey.swift
@@ -0,0 +1,15 @@
+//
+//  APIKey.swift
+//  NewsAppMVVM
+//
+//  Created by Muhammed Enes Kılıçlı on 17.09.2022.
+//
+
+import Foundation
+
+struct APIKey {
+    
+    static let key = "16035afb911b4a4281ccde6e3de9f0e0"
+    
+    
+}
